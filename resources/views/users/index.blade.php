16369972dfd991c06aef7d5d20e0b35b3a87c96a
JarellysAltamirano <jarellysltamirano@gmail.com>
2021-11-18 19:34:21 -0600

    trabajo retrasado

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <table class="table col-12">
+                <thead>
+                    <tr>
+                        <td>Id</td>
+                        <td>Usuario</td>
+                        <td>Correo</td>
+                        <td>Opciones</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($usuarios as $usuario)
+                        <tr>
+                            
+                            <td>{{ $usuario->id }}</td>
+                            <td>{{ $usuario->name }}</td>
+                            <td>{{ $usuario->email }}</td>
+                            <td>
+                               
+                                <form 
+                                    action="{{url('/users/'.$usuario->id)}}"
+                                    method="POST"
+                                    class="d-inline"
+                                >
+                                    @csrf
+                                    {{method_field('DELETE')}}
+                                    <button 
+                                        type="submit" 
+                                        onclick="return confirm('¿Desea eliminar este usuario?')"
+                                        value="delete"
+                                        class="btn btn-danger"
+                                    >Eliminar
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@endsection
